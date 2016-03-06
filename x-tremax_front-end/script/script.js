1a686c54d695b1008a67cc85d5b30b4c2e365b85
bintangadinandra <bintangadinandra@gmail.com>
2016-03-06 13:08:36 +0700

    json, dan scripting

--- x-tremax_front-end/script/script.js
+++ x-tremax_front-end/script/script.js
@@ -1,14 +1,19 @@
 //Initiate Google Map
-var data = JSON.parse("data.json");
-console.log(data[0].name);
+$(function(){
+    $.getJSON('data.json',function(data){
+        console.log('success');
+    })
+});
 
 function initMap(){
 	var mapDiv = document.getElementById('map');
 	var myLatLng = {lat:  1.334816, lng: 103.850744};
-	var myLatLng2 = {lat:  1.333733, lng: 103.850583}
+	var myLatLng2 = {lat:  1.333733, lng: 103.850583};
+  var myLatLng3 = {lat: 1.331798, lng: 103.851315};
+  var myLatLng4 = {lat: 1.33095, lng: 103.847633}
     var map = new google.maps.Map(mapDiv, {
-    	center: {lat:  1.334816, lng: 103.850744},
-      	zoom: 11
+    	center: myLatLng2,
+      	zoom: 16,
     });
 
     var marker1 = new google.maps.Marker({
@@ -22,4 +27,16 @@ function initMap(){
     map: map,
     title: 'Hello World!'
   	});
+
+    var marker3 = new google.maps.Marker({
+    position: myLatLng3,
+    map: map,
+    title: 'Hello World!'
+    });
+
+    var marker4 = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'Hello World!'
+    });
 }
