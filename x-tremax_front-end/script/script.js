8b7a948895ef799ab82e201f596a99ba8136e5a9
bintangadinandra <bintangadinandra@gmail.com>
2016-03-07 02:20:37 +0700

    infowindow final, mirip dengan mockup, perapihan font

--- x-tremax_front-end/script/script.js
+++ x-tremax_front-end/script/script.js
@@ -29,6 +29,8 @@ function initMap(){
 
   for (i in places){
     var infowindow = new google.maps.InfoWindow({
+      maxWidth: 250,
+      disableAutoPan: true
     });
 
     var marker = new google.maps.Marker({
@@ -41,40 +43,20 @@ function initMap(){
     google.maps.event.addListener(marker, 'click', 
       (function(marker, i) {
       return function() {
-      var popovercontent = "<div style='width: 250px; height:350px; margin: 0px; background:white'>" +
+      var popovercontent = "<div style='width: 100%; height:350px; margin: 0px; background:white;'>" +
       "<img src='img/" + places[i].image + ".jpg' style='width:100%; height:150px; object-fit: cover; '>" + 
-      "<h4 style='margin-left:10px'>" + places[i].name + "</h4>" +
+      "<h4 style='margin-left:10px; color: #a60b43;'>" + places[i].name + "</h4>" +
       "<div style='padding: 0px 10px; height: 120px; overflow-y: auto'>" +
       "<h5>Description</h5>" +
       "<p>Lorem ipsum dolor sit amet, ad facer eripuit commune sit. Eum platonem salutandi explicari no, pro accumsan suavitate conceptam te. Enim libris</p>" +
       "<h5>Gazzete Date</h5>" + 
       "<p>20 July</p>" +
       "</div></div>";
-      var boxText = document.createElement("div");
-      boxText.style.cssText = "border: 1px solid black; margin-top: 8px; background: yellow; padding: 5px;";
-      boxText.innerHTML = "City Hall, Sechelt<br>British Columbia<br>Canada";
 
-      var myOptions = {
-        alignBottom: true
-         ,content: popovercontent
-        ,disableAutoPan: false
-        ,maxWidth: 0
-        ,pixelOffset: new google.maps.Size(-140, 0)
-        ,zIndex: null
-        ,closeBoxMargin: "0px"
-        ,closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif"
-        ,infoBoxClearance: new google.maps.Size(1, 1)
-        ,isHidden: false
-        ,pane: "floatPane"
-        ,enableEventPropagation: false
-      };
-
-      var ib = new InfoBox(myOptions);
-
-      // infowindow.setContent(popovercontent);
-      // infowindow.open(map, marker);
-      ib.open(map,marker);
-      map.setCenter(marker.getPosition());
+      infowindow.setContent(popovercontent);
+      infowindow.open(map, marker);
+      // map.setCenter(marker.getPosition());
+      map.setCenter({lat: places[i].latitude+0.008, lng: places[i].longitude});
         if (map.getZoom!=15){
         map.setZoom(15);
       }
@@ -86,11 +68,56 @@ function initMap(){
 
     // The function to trigger the marker click, 'id' is the reference index to the 'markers' array.
   };
+
+  //This is a solution pure from the internet, with some modifications
+  //source: "http://en.marnoto.com/2014/09/5-formas-de-personalizar-infowindow.html"
+  google.maps.event.addListener(infowindow, 'domready', function() {
+
+     // Reference to the DIV which receives the contents of the infowindow using jQuery
+     var iwOuter = $('.gm-style-iw');
+
+     /* The DIV we want to change is above the .gm-style-iw DIV.
+      * So, we use jQuery and create a iwBackground variable,
+      * and took advantage of the existing reference to .gm-style-iw for the previous DIV with .prev().
+      */
+     var iwBackground = iwOuter.prev();
+
+     // Remove the background shadow DIV
+     iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+     // Remove the white background DIV
+     iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+     iwOuter.parent().parent().css({left: '15px'});
+     // Moves the shadow of the arrow 76px to the left margin 
+     iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 125px !important;'});
+
+     // Moves the arrow 76px to the left margin 
+     iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 125px !important;'});
+     iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
+     // Taking advantage of the already established reference to
+      // div .gm-style-iw with iwOuter variable.
+      // You must set a new variable iwCloseBtn.
+      // Using the .next() method of JQuery you reference the following div to .gm-style-iw.
+      // Is this div that groups the close button elements.
+      var iwCloseBtn = iwOuter.next();
+
+      // Apply the desired effect to the close button
+      iwCloseBtn.css({
+      opacity: '1', // by default the close button has an opacity of 0.7
+      right: '42px', top: '20px', // button repositioning
+      });
+
+      // The API automatically applies 0.7 opacity to the button after the mouseout event.
+      // This function reverses this event to the desired value.
+      iwCloseBtn.mouseout(function(){
+        $(this).css({opacity: '1'});
+      });
+  });
 }
 
 function myClick(id){
   $('html, body').animate({
-      scrollTop: $("#map").offset().top
+      scrollTop: $("#map").offset().top - 100
   }, 500);
   google.maps.event.trigger(map_marker[id], 'click');
 }
