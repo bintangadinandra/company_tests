1c4a320faacbcd9462514aff8e27bb310a037ae0
bintangadinandra <bintangadinandra@gmail.com>
2016-03-07 12:44:54 +0700

    Perapihan akhir: font, padding article, pagination

--- x-tremax_front-end/script/content.js
+++ x-tremax_front-end/script/content.js
@@ -1,11 +1,13 @@
+//Function to append articles and insert to HTML
 function loadarticles(cnt,targetID){
 	for (i in cnt){
-		var div = $("<div></div>", {class: 'col-sm-4'});
+		var div = $("<div></div>", {class: 'col-sm-4', style: 'padding:0px 26px'});
+		//add function to googlemap trigger marker click (see script.json, line 124)
 		var link = $("<a></a>", {href: '#', onclick: "myClick(" + i + "); return false"});
 		link.append($("<img>", {src: 'img/' + cnt[i].image + ".jpg", alt:'article-image'}));
 		div.append(link);
-		div.append($("<h3></h3>").text(cnt[i].name));
-		div.append($("<h4></h4>").html(cnt[i].tourtype + "<br>" +
+		div.append($("<h4></h4>").text(cnt[i].name));
+		div.append($("<p></p>", {style: "line-height:30px"}).html(cnt[i].tourtype + "<br>" +
 									   cnt[i].placetype + "<br>" +
 									   cnt[i].date));
 		$(targetID).append(div);
@@ -14,10 +16,12 @@ function loadarticles(cnt,targetID){
 
 
 $(document).ready(function(){
+	//Asychronously load DATA(places) JSON file
 	$.getJSON("script/data.json", function(result){
         loadarticles(result, "article");
     });
 
+    //Asychronously load ASIDE JSON file, and append the content
     $.getJSON("script/aside.json", function(result){
     	for (i in result){
 	    	var div = $("<div></div>");
--- x-tremax_front-end/script/script.js
+++ x-tremax_front-end/script/script.js
@@ -1,25 +1,28 @@
-//Initiate Google Map
-
-var places;
+//Initiate Global Var
 var map_marker = [];
 
+//---------------------------Load Data From JSON----------------------------
 //Sychronus
+var places;
 $.ajax({
     async: false,
     url: "script/data.json",
+    datatype: "json",
     success: function(json) {
         console.log("Sychronously Success");
         places = $.parseJSON(json);
     }
 });
 
-//AJAX
+//Asynchronus
 // $.getJSON("script/data.json", function(json) {
 //     // this will show the info it in firebug console
 
 // });
 
+//----------------------------Google Map------------------------------------
 function initMap(){
+  //Initiating Map, Center, and Zooms
 	var mapDiv = document.getElementById('map');
   var mapCenter = {lat:  1.331798, lng: 103.851315};
   var map = new google.maps.Map(mapDiv, {
@@ -27,19 +30,22 @@ function initMap(){
         zoom: 11,
   });
 
+  //Load places attributes (places is from data.json)
   for (i in places){
+    //Initiate Infowindow
     var infowindow = new google.maps.InfoWindow({
       maxWidth: 250,
       disableAutoPan: true
     });
 
+    //Create marker based on paces latitude and longitude
     var marker = new google.maps.Marker({
     position: {lat:  places[i].latitude, lng: places[i].longitude},
     map: map,
     icon: "img/" + i + ".png",
     title: places[i].name});
 
-    //Event Listener Whenever The User Click The Pin
+    //Event Listener Whenever The User Click The Marker
     google.maps.event.addListener(marker, 'click', 
       (function(marker, i) {
       return function() {
@@ -65,11 +71,10 @@ function initMap(){
 
     // Push the marker to the 'markers' array
     map_marker.push(marker);
-
-    // The function to trigger the marker click, 'id' is the reference index to the 'markers' array.
   };
 
-  //This is a solution pure from the internet, with some modifications
+  //INFOWINDOW CUSTOMIZATION
+  //This is a solution from the internet with modifications
   //source: "http://en.marnoto.com/2014/09/5-formas-de-personalizar-infowindow.html"
   google.maps.event.addListener(infowindow, 'domready', function() {
 
@@ -87,12 +92,12 @@ function initMap(){
 
      // Remove the white background DIV
      iwBackground.children(':nth-child(4)').css({'display' : 'none'});
-     iwOuter.parent().parent().css({left: '15px'});
+     iwOuter.parent().parent().css({left: '10px'});
      // Moves the shadow of the arrow 76px to the left margin 
-     iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 125px !important;'});
+     iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 130px !important;'});
 
      // Moves the arrow 76px to the left margin 
-     iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 125px !important;'});
+     iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 130px !important;'});
      iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
      // Taking advantage of the already established reference to
       // div .gm-style-iw with iwOuter variable.
@@ -115,30 +120,13 @@ function initMap(){
   });
 }
 
+//A function applied to all thumbnails, load marker's infowindow
 function myClick(id){
   $('html, body').animate({
       scrollTop: $("#map").offset().top - 100
   }, 500);
+  //trigger the map_marker's index and its action: click (show info-window)
   google.maps.event.trigger(map_marker[id], 'click');
-}
-
-function generatePop(marker, i) {
-      return function() {
-
-      var popovercontent = "<div style='width:100%; height:350px; border: 1px solid #eee;'>" +
-      "<img src='img/" + places[i].image + ".jpg' style='width:100%; height:150px; object-fit: cover; '>" + 
-      "<h4 style='margin-left:10px'>" + places[i].name + "</h4>" +
-      "<div style='padding: 0px 10px; height: 150px; overflow-y: auto'>" +
-      "<h5>Description</h5>" +
-      "<p>Lorem ipsum dolor sit amet, ad facer eripuit commune sit. Eum platonem salutandi explicari no, pro accumsan suavitate conceptam te. Enim libris</p>" +
-      "<h5>Gazzete Date</h5>" + 
-      "<p>20 July</p>" +
-      "</div></div>";
-         infowindow.setContent(popovercontent);
-         infowindow.open(map, marker);
-         map.setCenter(marker.getPosition());
-         map.setZoom(15);
-      }
 }
 
 initMap();
